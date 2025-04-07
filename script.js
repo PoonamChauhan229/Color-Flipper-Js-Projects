231fc89cea06ee87250528a3568b363a2f3dad5e
PoonamChauhan229 <poonam.chauhan229@gmail.com>
2025-04-07 19:07:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById("flip-btn");
+const colorCode = document.getElementById("color-code");
+
+function getRandomHexColor() {
+  const hex = Math.floor(Math.random() * 0xffffff).toString(16);
+  return "#" + hex.padStart(6, "0");
+}
+
+btn.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorCode.textContent = newColor;
+});
